62020b99d7f0fd786fb76678c338ddee9897b073
zhaoyenan90 <109201671+zhaoyenan90@users.noreply.github.com>
2025-11-11 13:00:28 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -105,8 +105,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["172.16.7.156"];
+pub const RS_PUB_KEY: &str = "LiGWJeXnzsmlp+mQDNW9i5u3w6m5UHw7LOjyLb8EAkg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
